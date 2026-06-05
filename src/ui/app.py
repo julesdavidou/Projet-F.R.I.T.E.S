a64d13adccd1ab81f2caec9e611661e8d5d4d67b
Athem2510 <pavuem@gmail.com>
2026-06-05 09:50:20 +0200

    modification app.py, problème avec chainlit resolu, il faut être sous python 3.11 !!

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,6 +1,9 @@
 import chainlit as cl
+import numpy as np
+import wave
 import uuid
-from src.agent.graph import graph
+from pathlib import Path
+#from src.agent.graph import graph
 
 @cl.on_chat_start
 async def start():
@@ -11,7 +14,7 @@ async def start():
     await cl.Message(
         content="Bienvenue sur L'Agent de sécurité de l'UPHF.\n Sentez-vous libre de lui demander ce que vous désirez savoir sur la cybersécurité"
     ).send() #message 
-
+"""
 @cl.on_message
 async def on_message(message: cl.Message):
     thread_id = cl.user_session.get("thread_id") #reprend l'id de session
@@ -29,3 +32,51 @@ async def on_message(message: cl.Message):
     response = result["messages"][-1].content
 
     await cl.Message(content=response).send() #reponse de l'ia
+"""
+@cl.on_chat_start
+async def start():
+    cl.user_session.set("audio_buffer", [])
+
+
+
+@cl.on_audio_chunk
+async def on_audio_chunk(chunk):
+    buffer = cl.user_session.get("audio_buffer")
+    buffer.append(chunk.data)
+
+    cl.user_session.set("audio_buffer", buffer)
+
+def save_pcm_to_wav(pcm_bytes: list[bytes], sample_rate=24000):
+
+    audio_dir = Path("tmp/audio")
+    audio_dir.mkdir(parents=True, exist_ok=True)
+
+    file_path = audio_dir / f"{uuid.uuid4()}.wav"
+
+    # concat chunks
+    raw_audio = b"".join(pcm_bytes)
+
+    # convert bytes -> int16 array
+    audio_array = np.frombuffer(raw_audio, dtype=np.int16)
+
+    # write wav
+    with wave.open(str(file_path), "wb") as wf:
+        wf.setnchannels(1)      # mono
+        wf.setsampwidth(2)      # int16 = 2 bytes
+        wf.setframerate(sample_rate)
+        wf.writeframes(audio_array.tobytes())
+
+    return file_path
+
+@cl.on_audio_end
+async def on_audio_end():
+
+    buffer = cl.user_session.get("audio_buffer")
+
+    wav_path = save_pcm_to_wav(buffer)
+
+    print("WAV généré :", wav_path)
+
+    text = transcribe(str(wav_path))
+
+    print("TRANSCRIPTION :", text)
